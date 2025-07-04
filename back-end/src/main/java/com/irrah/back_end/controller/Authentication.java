998e8fb1a80d993fcecd2144d4fe0aca15a0cf54
andresanchessouto <andresanchessouto@gmail.com>
2025-07-04 14:47:43 -0300

    startup configuration

---
+++ back-end/src/main/java/com/irrah/back_end/controller/Authentication.java
@@ -0,0 +1,12 @@
+package com.irrah.back_end.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class Authentication {
+
+
+}
